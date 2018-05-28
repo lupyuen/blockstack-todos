402fc87c4a849d4add6a66984ef02dca39b5ed54
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-05-28 15:38:23 +0800

    Added pkijs

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -2,21 +2,12 @@
 
 echo This is a test > /tmp/encrypt.txt
 
-## openssl rsautl -encrypt -inkey ~/.ssh/github_rsa.pub -pubin -in /tmp/encrypt.txt -out /tmp/encrypt.dat
-## openssl x509 -in ~/.ssh/github_rsa.pub -pubkey -noout >/tmp/public_key.pem
-
-## ssh-keygen -f ~/.ssh/github_rsa.pub -e -m pem >/tmp/public_key.pem
-## cat /tmp/public_key.pem
-
-## openssl rsautl -encrypt -inkey /tmp/public_key.pem -pubin -in /tmp/encrypt.txt -out /tmp/encrypt.dat
-
-## openssl rsautl -oaep -encrypt -inkey ~/.ssh/github_rsa.pub -pubin -in /tmp/encrypt.txt -out /tmp/encrypt.dat
-
 ssh-keygen -e -f ~/.ssh/github_rsa.pub -m PKCS8 > /tmp/public_key.pem
-
-## openssl rsa -in ~/.ssh/github_rsa.pub -pubout > /tmp/public_key.pem
 cat /tmp/public_key.pem
 
 openssl rsautl -encrypt -inkey /tmp/public_key.pkcs8 -pubin -in /tmp/encrypt.txt -out /tmp/encrypt.dat
 
-## openssl rsautl -encrypt -inkey /tmp/public_key.pem -pubin -in /tmp/encrypt.txt -out /tmp/encrypt.dat
+####
+
+ssh-keygen -e -f ~/.ssh/github_rsa.pub -m PKCS8 > /tmp/public_key.pem
+cat /tmp/public_key.pem
